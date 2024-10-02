55f1a620d9f7c97d5aa5411fcad4c0872b0740b3
LTSlw <38352100+LTSlw@users.noreply.github.com>
2024-10-02 21:47:33 +0800

    ch1: Support function call

--- src/entry.asm
+++ src/entry.asm
@@ -1,4 +1,12 @@
     .section .text.entry
     .globl _start
 _start:
-    li x1, 100
+    la sp, boot_start_top
+    call rust_main
+
+    .section .bss.stack
+    .globl boot_stack_lower_bound
+boot_stack_lower_bound:
+    .space 4096 * 16
+    .globl boot_start_top
+boot_start_top:
